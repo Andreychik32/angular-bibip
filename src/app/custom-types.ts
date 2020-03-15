029cbbc962fbf72c0fae3825cbc249641987f619
Andrii Yaroshenko <lavilaya@protonmail.ch>
2020-03-16 01:31:03 +0200

    fixed even/odd week bugs

--- src/app/custom-types.ts
+++ src/app/custom-types.ts
@@ -4,12 +4,36 @@ export interface LessonType {
 }
 
 export const getCurrentWeek = (): Week => {
-  const date = new Date();
-  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
-  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7) % 2 === 0
-    ? Week.Even
-    : Week.Odd;
+  const instance = new Date(2020, 2, 16, 0, 0, 0, 0);
+
+  // Create a copy of this date object
+  const target = new Date(instance.valueOf());
+
+  // ISO week date weeks start on monday
+  // so correct the day number
+  const dayNr = (instance.getDay() + 6) % 7;
+
+  // ISO 8601 states that week 1 is the week
+  // with the first thursday of that year.
+  // Set the target date to the thursday in the target week
+  target.setDate(target.getDate() - dayNr + 3);
+
+  // Store the millisecond value of the target date
+  const firstThursday = target.valueOf();
+
+  // Set the target to the first thursday of the year
+  // First set the target to january first
+  target.setMonth(0, 1);
+  // Not a thursday? Correct the date to the next thursday
+  if (target.getDay() !== 4) {
+    target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
+  }
+
+  // The weeknumber is the number of weeks between the
+  // first thursday of the year and the thursday in the target week
+  const weekNumber =
+    1 + Math.ceil((firstThursday - target.getTime()) / 604800000);
+  return weekNumber % 2 === 0 ? Week.Even : Week.Odd;
 };
 
 export class Lesson {
@@ -23,24 +47,6 @@ export class Lesson {
     if (lesson2) {
       this.secondLesson = lesson2;
     }
-  }
-
-  get title() {
-    if (this.secondLesson) {
-      return getCurrentWeek() === Week.Odd
-        ? this.firstLesson.title
-        : this.secondLesson.title;
-    }
-    return this.firstLesson.title;
-  }
-
-  get info() {
-    if (this.secondLesson) {
-      return getCurrentWeek() === Week.Odd
-        ? this.firstLesson.info
-        : this.secondLesson.info;
-    }
-    return this.firstLesson.info;
   }
 }
 
