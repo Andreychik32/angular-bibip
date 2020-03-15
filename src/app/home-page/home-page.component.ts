b79828db3a41a6eae4e8a7c5b3e8eade5c0d2930
Andrii Yaroshenko <lavilaya@protonmail.ch>
2020-03-15 23:38:37 +0200

    added title, metatags and PWA support

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ScheduleService } from "../schedule.service";
 import { Day, LessonType, Lesson } from "../custom-types";
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: "app-home-page",
@@ -12,7 +13,44 @@ export class HomePageComponent {
   parseInt = parseInt;
   selectedDay: Day;
 
-  constructor(private scheduleService: ScheduleService) {}
+  constructor(
+    private scheduleService: ScheduleService,
+    private meta: Meta,
+    private titleService: Title
+  ) {
+    this.titleService.setTitle("Бібіп");
+    this.meta.addTags([
+      { name: "title", content: "Бібіп" },
+      {
+        name: "description",
+        content: "Розклад занять для однієї із груп університету факультету ІТ."
+      },
+
+      { name: "og:type", content: "website" },
+      { name: "og:url", content: "https://bibip.haxellio.me/" },
+      { name: "og:title", content: "Бібіп" },
+      {
+        name: "og:description",
+        content: "Розклад занять для однієї із груп університету факультету ІТ."
+      },
+      {
+        name: "og:image",
+        content: "https://bibip.haxellio.me/assets/icons/icon-512x512.png"
+      },
+
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:url", content: "https://bibip.haxellio.me/" },
+      { name: "twitter:title", content: "Бібіп" },
+      {
+        name: "twitter:description",
+        content: "Розклад занять для однієї із груп університету факультету ІТ."
+      },
+      {
+        name: "twitter:image",
+        content: "https://bibip.haxellio.me/assets/icons/icon-512x512.png"
+      }
+    ]);
+  }
 
   get lessons() {
     return this.scheduleService.getLessons;
