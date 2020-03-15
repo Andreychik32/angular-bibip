109e98de19dbf292a8fb551e8db894c5008f56f7
Andrii Yaroshenko <lavilaya@protonmail.ch>
2020-03-15 17:52:54 +0200

    added bells and fixed typo

--- src/app/schedule.service.ts
+++ src/app/schedule.service.ts
@@ -6,12 +6,13 @@ import { Lesson, Day } from "./custom-types";
 })
 export class ScheduleService {
   private lessons: Map<Day, { [key: number]: Lesson }>;
+  private bells: Array<string>;
 
   constructor() {
     this.lessons = new Map<Day, { [key: number]: Lesson }>();
     this.lessons.set(Day.Monday, {
       2: {
-        title: "Дискретні страктури даних",
+        title: "Дискретні структури даних",
         info: "каб. 230, корп. 15"
       },
       3: {
@@ -75,9 +76,24 @@ export class ScheduleService {
         info: "каб. 231 корп. 15"
       }
     });
+
+    this.bells = new Array<string>(
+      "8:30 - 9:50",
+      "10:05 - 11:25",
+      "11:40 - 13:00",
+      "13:15 - 14:35",
+      "14:50 - 16:10",
+      "16:25 - 17:45",
+      "18:00 - 19:20",
+      "19:35 - 20:55"
+    );
   }
 
   get getLessons() {
     return this.lessons;
+  }
+
+  get getBells() {
+    return this.bells;
   }
 }
